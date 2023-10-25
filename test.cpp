1a1ccec04c251d13575d491ecb02fced81835069
Septchi <septchi@Ubuntu.myguest.virtualbox.org>
2023-10-26 07:19:38 +0800

    pre multi-digit multiplication

--- test.cpp
+++ test.cpp
@@ -32,7 +32,7 @@ Digit* newDigit(int n){
 
 int compare(Digit* d1, Digit* d2){
 	if(d1->val > d2->val) return 1;
-	else if(d1->val > d2->val) return -1;
+	else if(d1->val < d2->val) return -1;
 	return 0;
 }
 Digit* add(Digit* d1, Digit* d2){
@@ -62,32 +62,43 @@ Digit* add(Digit* d1, Digit* d2){
 }
 //assume d1 > d2
 Digit* sub(Digit* d1, Digit* d2){
-	Digit* d;
+	Digit* d = newDigit(0);
+  if(!d2){
+    d = d1;
+    return d;
+  }
 	int diff = oct(d1->val-d2->val);
 	d = newDigit(diff);
-	if(d2->val>d1->val){
-		d1->right->val -= 1;
-	}
-	if(d1->right || d2->right){
+	if(d1->right){
 		d->right = sub(d1->right, d2->right);
 		d->right->left = d;
+    if(d2->val>d1->val)
+      d->right->val -= 1;
 	}
+  if(d->right)
+    if(d->right->val == 0)
+      d->right = nullptr;
 	return d;
 }
 Digit* mult(Digit* d1, Digit* d2){
-	Digit* d;
+	Digit* d = newDigit(0);
 	Digit* mind = compare(d1, d2) == -1 ? d1 : d2; 
 	Digit* maxd = compare(d1, d2) != -1 ? d1 : d2; 
+  if(mind->val == 0) return d;
+  if(mind->val == 1) return maxd;
 	Digit* a = newDigit(7/mind->val); 
 	Digit* b = newDigit(maxd->val/a->val); 
-	d->right = b;
 	if(b->val == 0){
 		d->val = mind->val*maxd->val;
 		return d;
 	}
+	d->right = b;
 	Digit* temp = newDigit(7%mind->val+1);
 	d = sub(d, mult(temp, b));
-	temp =
+	a = newDigit(7/mind->val); 
+	b = newDigit(maxd->val/a->val); 
+	temp = sub(maxd,  mult(a, b));
+  d = add(d, mult(temp, mind));
 	return d;
 }
 
@@ -128,11 +139,19 @@ Num subNum(Num n1, Num n2){
 	return newNum(d);
 }
 
+Num multNum(Num n1, Num n2){
+  function<Digit* (Digit*, Digit*)> func = [&](Digit* d1, Digit* d2){
+    return mult(d1, d2);
+  };
+  Digit* d = mult(&n1.lsb, &n2.lsb);
+  return newNum(d);
+}
+
 int main(){
-  Num n1 = newNum(newDigit(15));
-  Num n2 = newNum(newDigit(10));
+  Num n1 = newNum(newDigit(8));
+  Num n2 = newNum(newDigit(7));
   Num res;
-  res = subNum(n1, n2);
+  res = multNum(n1, n2);
   cout << "n1: ";
   show(n1);
   cout << "n2: ";
